845d4f150e67062f6a90ab4de69f3f0849496509
ownspaceegorova <own.space.egorova@yandex.ru>
2023-04-21 17:08:19 +0500

    правки

---
+++ xo-panel.js
@@ -0,0 +1,11 @@
+let game = document.getElementById('game-container');
+let board = document.getElementById('game-board');
+let cell = document.querySelectorAll('.cell');
+for (i = 0; cell.length > i; i++) {
+    cell[i].onclick = function () {
+        alert("jhb")
+
+    }
+    
+}
+
