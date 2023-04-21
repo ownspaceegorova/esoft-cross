3ee8fba79816db440fdec42dce2bb4fee6be9c94
ownspaceegorova <own.space.egorova@yandex.ru>
2023-04-21 20:20:38 +0500

    ходы игроков

--- xo-panel.js
+++ xo-panel.js
@@ -1,11 +1,21 @@
-let game = document.getElementById('game-container');
-let board = document.getElementById('game-board');
-let cell = document.querySelectorAll('.cell');
-for (i = 0; cell.length > i; i++) {
-    cell[i].onclick = function () {
-        alert("jhb")
-
+const cells = document.querySelectorAll('.cell');
+let clickCount = 0;
+cells.forEach((cells) => {
+  const child = cells.querySelector('.hod');
+  cells.addEventListener('click', () => {
+    if (!cells.classList.contains('cross-active') && !cells.classList.contains('zero-active')) {
+    clickCount++;
+    if (clickCount % 2 === 0) {
+      child.classList.add('zero-active');
+      cells.classList.add('zero-active');
+    } else {
+      child.classList.add('cross-active');
+      cells.classList.add('cross-active');
     }
-    
+
 }
+
+  });
+});
+
 
