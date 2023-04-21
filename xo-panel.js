9bb687d9f55b0b51be7e6377f67ee939e7e70fc3
ownspaceegorova <own.space.egorova@yandex.ru>
2023-04-21 20:52:49 +0500

    замена игрока

--- xo-panel.js
+++ xo-panel.js
@@ -2,20 +2,22 @@ const cells = document.querySelectorAll('.cell');
 let clickCount = 0;
 cells.forEach((cells) => {
   const child = cells.querySelector('.hod');
+
   cells.addEventListener('click', () => {
     if (!cells.classList.contains('cross-active') && !cells.classList.contains('zero-active')) {
     clickCount++;
     if (clickCount % 2 === 0) {
       child.classList.add('zero-active');
       cells.classList.add('zero-active');
+      document.getElementById('game-step').innerHTML='Ходит&nbsp;<img src="image/x.svg"/>&nbsp;Екатерина Плюшкина';
     } else {
       child.classList.add('cross-active');
       cells.classList.add('cross-active');
+      document.getElementById('game-step').innerHTML='Ходит&nbsp;<img src="image/zero.svg"/>&nbsp;Владлен Пупкин';
     }
-
 }
-
   });
 });
+
 
 
